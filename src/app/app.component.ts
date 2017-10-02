67008d661a5b824a32d577d0edc4b4a4881d6b0f
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-10-02 13:47:43 +0530

    App Component & index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
+//Import - Allows us to use exported members from external ES modules/angular modules
 import { Component } from '@angular/core';
+//Eg. @angular/core, @angular/animate, @angular/http, @angular/router
 
+//Metadata - Start
+//A class becomes a component when we define a metadata
+//Decorator - A function that adds metadata to a class, its members or its arguments. Prefixed with @. Eg.@Component()
 @Component({
+  //selector defines a directive name. A directive is a custom HTML tag.
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //Template - Start
+  template: `
+    <div><h1>{{pageTitle}}</h1>
+        <div>My First Component</div>
+    </div>
+  `//Backtick (Not Quotes)
+  //templateUrl: './app.component.html',
+  //Template - End
+  //styleUrls: ['./app.component.css']
 })
+//Notice there is no semicolon here after ).
+//Metadata - End
+
+//Class - Start
+//As this file is exporting something, it is an ES2015 module
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  //Property
+  //title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management';
 }
+//Class - End
