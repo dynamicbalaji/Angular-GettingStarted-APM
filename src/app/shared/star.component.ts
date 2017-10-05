2c90fc4a582985f000a388e81dc78728b77a4ec0
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-10-05 06:59:37 +0530

    Nested components - Passing data back to container component - @Output

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnChanges, Input} from '@angular/core';
+import { Component, OnChanges, Input, EventEmitter, Output } from '@angular/core';
 
 @Component({
     selector:'pm-star',
@@ -12,6 +12,22 @@ export class StarComponent implements OnChanges{
     //Data to be passed from Container component to Nested component - @Input PROPERTY
     @Input() rating: number;
     starWidth: number;
+
+    //Data to be passed from Nested component to Container component - @Output EVENT
+    //Output decorator can be used to decorate any property of the nested component - property type must be an event
+    //This is the only way that a nested component can pass data to its container
+    //The data to pass becomes the event payload
+    //In Angular, an event is defined wih an event emitter object - it can be an instance of any datatype
+    @Output() ratingClicked: EventEmitter<string>
+                    = new EventEmitter<string>();
+
+
+    onClick(): void {
+        //ES2015 back ticks to define a template string
+        console.log(`The rating ${this.rating} clicked`);
+        //We are passing a string in this emit method, so the EventEmitter is having generic as string
+        this.ratingClicked.emit(`The rating ${this.rating} clicked`);
+    }
 
     ngOnChanges(): void {
         //86 is the width of the star div width in html
