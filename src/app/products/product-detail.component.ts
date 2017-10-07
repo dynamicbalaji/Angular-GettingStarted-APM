b9c0238c177376bbfe6430846aea72a0d32cf6d4
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-10-07 12:38:15 +0530

    Passing parameters to a route

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { IProduct } from './product';
 
@@ -15,9 +16,32 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
   product: IProduct;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute) { 
+    /*
+      There are 2 different ways to get the parameter from route.
+      1. Snapshot - if only need to get the initial value of the parameter
+      2. Observable - if parameter will change without leaving the page For eg. If wee had a next button
+          on the product detail page to display the next product, the URL will change to the next
+          product's id so you'd want to use an observable instead.
+    */
+    console.log(this._route.snapshot.paramMap.get('id'));
+  }
 
   ngOnInit() {
+    //The plus in JS shortcut to convert the parameter string to a numeric id
+    let id = +this._route.snapshot.paramMap.get('id'); 
+    this.pageTitle += `: ${id}`;
+    console.log(this.pageTitle);
+    this.product = {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    };
   }
 
 }
